3b705a761d0f0f116a8e4409a154545543a91ca0
kapau <kapau@Kapauli98>
2024-07-16 19:47:37 -0600

    Incio del proyecto

---
+++ src/main/java/com/PatSolutions/ProyectoFinalG7Application.java
@@ -0,0 +1,13 @@
+package com.PatSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalG7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalG7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/PatSolutions/PatSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PatSolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
